4603cd7da3596d7a7c799b7135044b413285b6fd
Ivan <ivanchernetskey@mail.ru>
2021-06-16 18:02:15 +0300

    added styles from materialize and index.css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Navbar: React.FunctionComponent = () => {
     return(
         <nav>
-            <div className="nav-wrapper">
-                <a href="#" className="brand-logo right">React + TypScript</a>
+            <div className="nav-wrapper #42a5f5 blue lighten-1 px1">
+                <a href="/" className="brand-logo right">React + TypScript</a>
                 <ul className="left hide-on-med-and-down">
                     <li><a href="/">Список дел</a></li>
                     <li><a href="/">Информация</a></li>
